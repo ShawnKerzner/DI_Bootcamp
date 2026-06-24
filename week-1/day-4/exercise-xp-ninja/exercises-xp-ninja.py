5d0692d25ac65303f2772438eebe22ed269e90a4
Shawn Kerzner <shawnkerz98@gmail.com>
2026-06-24 13:16:05 +0300

    working on exercise 2

--- week-1/day-4/exercise-xp-ninja/exercises-xp-ninja.py
+++ week-1/day-4/exercise-xp-ninja/exercises-xp-ninja.py
@@ -1,15 +1,27 @@
 import math
 # Exercise 1:
 # Q = Square root of [(2 * C * D)/H]
-c = int(50)
-h = int(30)
-result = []
-user_input = input("Please enter 3 numbers seperated by commas: ")
-print(user_input)
-numbers = [int(num) for num in user_input.split(",")]
-print(numbers)
-for d in numbers:
-    q = int(math.sqrt((2 * c * d) // h))
-    result.append(q)
-    continue
-print(result)
+# c = int(50)
+# h = int(30)
+# result = []
+# user_input = input("Please enter 3 numbers seperated by commas: ")
+# print(user_input)
+# numbers = [int(num) for num in user_input.split(",")]
+# print(numbers)
+# for d in numbers:
+#     q = int(math.sqrt((2 * c * d) // h))
+#     result.append(q)
+#     continue
+# print(result)
+
+# Exercise 2
+# step 1
+integers_list = [3, 47, 99, -80, 22, 97, 54, -23, 5, 7]
+# step 2
+print(integers_list)
+sorted_list = sorted(integers_list)
+sorted_list.reverse()
+print(sorted_list)
+list_sum = sum(integers_list)
+print(list_sum)
+# step 3 
