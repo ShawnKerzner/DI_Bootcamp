868cd215d7215c04eb68f9f739c89a7343c62cac
Shawn Kerzner <shawnkerz98@gmail.com>
2026-07-02 18:30:36 +0300

    exercise 4 complete

--- week-1/Dictionaries/exercise-xp/exercises-xp.py
+++ week-1/Dictionaries/exercise-xp/exercises-xp.py
@@ -33,49 +33,64 @@
 #         print(f"{key}'s ticket costs $15")
 # print(f"The total cost is ${total_cost}")
 
-# Exercise 3
-brand = {
-    "name": "zara",
-    "creation_date": 1975,
-    "creator_name": "Amancio Ortega Gaona",
-    "type_of_clothes": "men, women, children, home",
-    "international_competitors": ["Gap", "H&M", "Benetton"],
-    "number_stores": 7000,
-    "major_color": {
-        "France": "blue",
-        "Spain": "red",
-        "US": ["pink", "green"]
-    }
-}
+# # Exercise 3
+# brand = {
+#     "name": "zara",
+#     "creation_date": 1975,
+#     "creator_name": "Amancio Ortega Gaona",
+#     "type_of_clothes": "men, women, children, home",
+#     "international_competitors": ["Gap", "H&M", "Benetton"],
+#     "number_stores": 7000,
+#     "major_color": {
+#         "France": "blue",
+#         "Spain": "red",
+#         "US": ["pink", "green"]
+#     }
+# }
 
-brand["number_stores"] = 2
-print("number_stores:", brand["number_stores"])
+# brand["number_stores"] = 2
+# print("number_stores:", brand["number_stores"])
 
-print(f"Zara's clients like styles for {brand['type_of_clothes']}.")
+# print(f"Zara's clients like styles for {brand['type_of_clothes']}.")
 
-brand["country_creation"] = "Spain"
-print(brand)
+# brand["country_creation"] = "Spain"
+# print(brand)
 
-if "international_competitors" in brand:
-    print("the key 'international_competitors' exists")
-else:
-    print("key not found")
+# if "international_competitors" in brand:
+#     print("the key 'international_competitors' exists")
+# else:
+#     print("key not found")
 
-brand["international_competitors"].append("Desigual")
-print("international_competitors:", brand["international_competitors"])
+# brand["international_competitors"].append("Desigual")
+# print("international_competitors:", brand["international_competitors"])
 
-del brand["creation_date"]
-print(brand)
+# del brand["creation_date"]
+# print(brand)
 
-print(brand["international_competitors"][-1])
+# print(brand["international_competitors"][-1])
 
-print(brand["major_color"]["US"])
-print(brand.keys())
+# print(brand["major_color"]["US"])
+# print(brand.keys())
 
-more_on_zara = {
-    "creation_date": 1975,
-    "number_stores": 7000
-}
+# more_on_zara = {
+#     "creation_date": 1975,
+#     "number_stores": 7000
+# }
 
-merged_brand = brand | more_on_zara
-print(merged_brand)
+# merged_brand = brand | more_on_zara
+# print(merged_brand)
+
+# Exercise 4
+users = ["Mickey", "Minnie", "Donald", "Ariel", "Pluto"]
+
+user_to_index = {user: index for index, user in enumerate(users)}
+print(user_to_index)
+
+index_to_user = {index: user for index, user in enumerate(users)}
+print(index_to_user)
+
+users.sort()
+print(users)
+
+sorted_users = {user: index for index, user in enumerate(sorted(users))}
+print(sorted_users)
