ac19fa2bcc2ec13330cefbf1838fad8859c84e97
Shawn Kerzner <shawnkerz98@gmail.com>
2026-07-02 16:21:30 +0300

    exercise 2 base completed

--- week-1/Dictionaries/exercise-xp/exercises-xp.py
+++ week-1/Dictionaries/exercise-xp/exercises-xp.py
@@ -3,3 +3,18 @@ keys = ['Ten', 'Twenty', 'Thirty']
 values = [10, 20, 30]
 result = dict(zip(keys, values))
 print(result)
+
+# Execise 2
+total_cost = 0
+family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+for key, value in family.items():
+    if value < 3:
+        print(f"{key}'s ticket is free")
+    elif value > 3 and value < 12:
+        total_cost += 10
+        print(f"{key}'s ticket costs $10")
+    elif value >= 12:
+        total_cost += 15
+        print(f"{key}'s ticket costs $15")
+print(f"The total cost is ${total_cost}")
+    
