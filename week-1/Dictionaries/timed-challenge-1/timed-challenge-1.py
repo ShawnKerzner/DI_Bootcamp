7ca910054a1d1705f7daf92d6f0e12d7197c0b22
Shawn Kerzner <shawnkerz98@gmail.com>
2026-07-07 15:16:47 +0300

    challenge complete

---
+++ week-1/Dictionaries/timed-challenge-1/timed-challenge-1.py
@@ -0,0 +1,6 @@
+reverse_input = input()
+input_list = reverse_input.split(" ")
+reversed_list = list(reversed(input_list))
+reversed = " ".join(reversed_list)
+print(reversed) 
+# Time remaining: 18:31
