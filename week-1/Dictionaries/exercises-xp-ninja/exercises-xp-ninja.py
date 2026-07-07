2e555a1e534ccb6df4815e063f824dcc6c7eac35
Shawn Kerzner <shawnkerz98@gmail.com>
2026-07-07 14:55:38 +0300

    all exercises completed

--- week-1/Dictionaries/exercises-xp-ninja/exercises-xp-ninja.py
+++ week-1/Dictionaries/exercises-xp-ninja/exercises-xp-ninja.py
@@ -1,14 +1,14 @@
-# car_brands = "Volkswagen, Toyota, Ford Motor, Honda, Chevrolet."
-# print(car_brands.split(","))
-# print(f"There are {len(car_brands.split(","))} car brands in this list")
-# car_brands_reverse = sorted(car_brands.split(","), reverse=True)
-# print(car_brands_reverse)
-# car_brands_list = [brand.strip(" .") for brand in car_brands.split(",")]
-# print(car_brands_list)
-# brands_with_o = [brand for brand in car_brands_list if "o" in brand.lower()]
-# print(f"There are {len(brands_with_o)} car brands with the letter 'o'.")
-# brands_without_i = [brand for brand in car_brands_list if "i" not in brand.lower()]
-# print(f"There are {len(brands_without_i)} car brands without the letter 'i'.")
+car_brands = "Volkswagen, Toyota, Ford Motor, Honda, Chevrolet."
+print(car_brands.split(","))
+print(f"There are {len(car_brands.split(","))} car brands in this list")
+car_brands_reverse = sorted(car_brands.split(","), reverse=True)
+print(car_brands_reverse)
+car_brands_list = [brand.strip(" .") for brand in car_brands.split(",")]
+print(car_brands_list)
+brands_with_o = [brand for brand in car_brands_list if "o" in brand.lower()]
+print(f"There are {len(brands_with_o)} car brands with the letter 'o'.")
+brands_without_i = [brand for brand in car_brands_list if "i" not in brand.lower()]
+print(f"There are {len(brands_without_i)} car brands without the letter 'i'.")
 
 # Bonus 1
 car_list = ["Honda","Volkswagen", "Toyota", "Ford Motor", "Honda", "Chevrolet", "Toyota"]
@@ -18,3 +18,8 @@ print(car_string)
 print(f"There are {len(set(car_list))} companies now in the list")
 
 # Bonus 2
+manufacturers = ["Honda","Volkswagen", "Toyota", "Ford Motor", "Honda", "Chevrolet", "Toyota"]
+sorted_manufacturers = sorted(manufacturers)
+print(sorted_manufacturers)
+reverse_words_sorted = [manufacturer[::-1] for manufacturer in sorted_manufacturers]
+print(reverse_words_sorted)
