aa0f723b96e726ccdd6a537340acd05723aac4b8
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-02 12:51:57 +0300

    ready for submission

--- week-1/day-1/classwork/classwork.py
+++ week-1/day-1/classwork/classwork.py
@@ -1,15 +1,29 @@
-def find_2():
+# def find_2():
+#     user_input = input("Please type a string: ")
+#     counter = 0
+#     if len(user_input) < 1:
+#         raise ValueError("String can not be empty")
+
+#     for char in user_input:
+#         if char == "2":
+#             counter += 1
+#     sum_of_counters = counter * int(char)
+#     print(sum_of_counters)
+#     return sum_of_counters
+
+
+# find_2()
+
+
+def sum_digits_in_string():
     user_input = input("Please type a string: ")
-    counter = 0
-    if len(user_input) < 1:
-        raise ValueError("String can not be empty")
-
+    digits = []
     for char in user_input:
-        if char == "2":
-            counter += 1
-    sum_of_counters = counter * int(char)
-    print(sum_of_counters)
-    return sum_of_counters
+        if char.isdigit():
+            digits.append(int(char))
+    sum_of_digits = sum(digits)
+    print(sum_of_digits)
+    return (sum_of_digits)
 
 
-find_2()
+sum_digits_in_string()
