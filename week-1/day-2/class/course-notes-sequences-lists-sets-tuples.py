777783870baace49bb95dc42646af270afdf1e79
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-02 16:34:31 +0300

    exercises in the class work

---
+++ week-1/day-2/class/course-notes-sequences-lists-sets-tuples.py
@@ -0,0 +1,22 @@
+# List Exercise
+list1 = [5, 10, 15, 20, 25, 50, 20]
+print(list1.index(20))
+list1[3] = 200
+print(list1)
+
+# Tuple Exercise
+a_tuple = (10, 20, 30, 40)
+a, b, c, d = a_tuple
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Sets
+my_set = set()
+my_set.add("Rick")
+my_set.add("Morty")
+print(my_set)
+
+this_set = {"banana", "apple", "cherry", "apple"}
+print(this_set)
