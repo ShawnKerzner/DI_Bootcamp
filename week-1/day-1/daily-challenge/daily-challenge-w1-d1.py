75bf9784e955805a088d8f17e4e694b9ae4b97f1
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-02 14:13:54 +0300

    adjusted code according to feedback

--- week-1/day-1/daily-challenge/daily-challenge-w1-d1.py
+++ week-1/day-1/daily-challenge/daily-challenge-w1-d1.py
@@ -9,16 +9,14 @@ def build_up_a_string():
         print("String too long.")
     else:
         print("Perfect string")
-    print(user_input[0], user_input[-1])
-    characters = []
-    for char in user_input:
-        characters.append(char)
-        result = " ".join(characters)
-        print(result)
-    input_to_list = list(user_input)
-    random.shuffle(input_to_list)
-    shuffled_text = "".join(input_to_list)
-    print(shuffled_text)
+        print(user_input[0], user_input[-1])
+        characters = []
+        for i in range(0, len(user_input)):
+            print(user_input[0:i+1])
+        input_to_list = list(user_input)
+        random.shuffle(input_to_list)
+        shuffled_text = "".join(input_to_list)
+        print(shuffled_text)
 
 
 build_up_a_string()
