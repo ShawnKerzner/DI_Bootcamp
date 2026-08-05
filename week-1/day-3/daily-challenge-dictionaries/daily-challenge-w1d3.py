1c46b43fc7e4691c0ba964493fe9886a6180dc18
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-05 09:57:18 +0300

    additional task added to exercise 1

--- week-1/day-3/daily-challenge-dictionaries/daily-challenge-w1d3.py
+++ week-1/day-3/daily-challenge-dictionaries/daily-challenge-w1d3.py
@@ -1,21 +1,32 @@
 # Exercise 1
-dictionary = {}
+dictionary1 = {}
+dictionary2 = {}
 user_input = input("Please enter a word: ")
 for index, char in enumerate(user_input):
-    if char not in dictionary:
-        dictionary[char] = [index]
+    if char not in dictionary1:
+        dictionary1[char] = [index]
     else:
-        dictionary[char].append(index)
-print(dictionary)
+        dictionary1[char].append(index)
+print(dictionary1)
+
+for char in user_input:
+    if char in dictionary2:
+        dictionary2[char] += 1
+    else:
+        dictionary2[char] = 1
+print(dictionary2)
 
 # Exercise 2
-# items_purchase = {"Water": "$1", "Bread": "$3", "TV": "$1,000", "Fertilizer": "$20"}
-# wallet = "$300"
+items_purchase = {"Water": "$1", "Bread": "$3",
+                  "TV": "$1,000", "Fertilizer": "$20"}
+wallet = "$300"
 
-# items_purchase = {"Apple": "$4", "Honey": "$3", "Fan": "$14", "Bananas": "$4", "Pan": "$100", "Spoon": "$2"}
-# wallet = "$100"
+items_purchase = {"Apple": "$4", "Honey": "$3", "Fan": "$14",
+                  "Bananas": "$4", "Pan": "$100", "Spoon": "$2"}
+wallet = "$100"
 
-items_purchase = {"Phone": "$999", "Speakers": "$300", "Laptop": "$5,000", "PC": "$1200"}
+items_purchase = {"Phone": "$999", "Speakers": "$300",
+                  "Laptop": "$5,000", "PC": "$1200"}
 wallet = "$1"
 
 basket = []
@@ -38,6 +49,5 @@ for key, value in items_purchase.items():
 if basket == []:
     print("Nothing")
 else:
-    print(f"The following items are in your basket {sorted(basket)} and you have ${new_wallet} left over")
-
-    
+    print(
+        f"The following items are in your basket {sorted(basket)} and you have ${new_wallet} left over")
