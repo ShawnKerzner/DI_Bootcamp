c11ee61d70e2850cda724222f9940276693c583d
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-06 11:28:18 +0300

    player input function complete

--- week-1/day-4/mini-project-tic-tac-toe/tic-tac-toe.py
+++ week-1/day-4/mini-project-tic-tac-toe/tic-tac-toe.py
@@ -1,4 +1,4 @@
-grid = [[" ", " ", " "], [" ", " ", ""], [" ", " ", " "]]
+grid = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
 
 def display_board(grid):
@@ -6,10 +6,23 @@ def display_board(grid):
     print("*" * 17)
     print(f"*   {grid[0][0]} | {grid[0][1]} | {grid[0][2]}   *")
     print("*  ---|---|---  *")
-    print(f"*   {grid[1][0]} | {grid[1][1]} | {grid[1][2]}    *")
+    print(f"*   {grid[1][0]} | {grid[1][1]} | {grid[1][2]}   *")
     print("*  ---|---|---  *")
     print(f"*   {grid[2][0]} | {grid[2][1]} | {grid[2][2]}   *")
     print("*" * 17)
 
 
+def player_input(player):
+    while True:
+        select_row = int(input("Enter row: "))
+        select_column = int(input("Enter column: "))
+        marked = grid[select_row][select_column]
+        if marked != " ":
+            print("This spot is already taken")
+        else:
+            grid[select_row][select_column] = "X" if player == 1 else "O"
+            break
+
+
+player_input(1)
 display_board(grid)
