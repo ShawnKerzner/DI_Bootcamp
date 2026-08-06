b21fea5bd4e349214d382bd13d301c407bfcac9f
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-06 12:05:14 +0300

    check_win function completed. Not sure how I am going to test this yet. might have to wait for the whole game to be  finished

--- week-1/day-4/mini-project-tic-tac-toe/tic-tac-toe.py
+++ week-1/day-4/mini-project-tic-tac-toe/tic-tac-toe.py
@@ -26,3 +26,38 @@ def player_input(player):
 
 player_input(1)
 display_board(grid)
+
+
+def check_win(board, player):
+    if player == 1:
+        for space in board:
+            if board[0][0] == "X" and board[0][1] == "X" and board[0][2]:
+                return True
+            elif board[1][0] == "X" and board[1][1] == "X" and board[1][2]:
+                return True
+            elif board[2][0] == "X" and board[2][1] == "X" and board[2][2]:
+                return True
+            elif board[0][0] == "X" and board[1][0] == "X" and board[2][0]:
+                return True
+            elif board[0][1] == "X" and board[1][1] == "X" and board[2][1]:
+                return True
+            elif board[0][2] == "X" and board[1][2] == "X" and board[2][2]:
+                return True
+            else:
+                return False
+    if player == 2:
+        for space in board:
+            if board[0][0] == "O" and board[0][1] == "O" and board[0][2]:
+                return True
+            elif board[1][0] == "O" and board[1][1] == "O" and board[1][2]:
+                return True
+            elif board[2][0] == "O" and board[2][1] == "O" and board[2][2]:
+                return True
+            elif board[0][0] == "O" and board[1][0] == "O" and board[2][0]:
+                return True
+            elif board[0][1] == "O" and board[1][1] == "O" and board[2][1]:
+                return True
+            elif board[0][2] == "O" and board[1][2] == "O" and board[2][2]:
+                return True
+            else:
+                return False
