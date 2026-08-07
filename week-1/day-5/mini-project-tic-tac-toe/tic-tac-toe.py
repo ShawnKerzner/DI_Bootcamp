915501be19e28dc122421773256929dd3812af55
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-07 18:30:06 +0300

    Fixed bug in check win function where the check would end after the first combo check

--- week-1/day-5/mini-project-tic-tac-toe/tic-tac-toe.py
+++ week-1/day-5/mini-project-tic-tac-toe/tic-tac-toe.py
@@ -52,45 +52,43 @@ def player_input(player):
 
 def check_win(board, player):
     if player == 1:
-        for space in board:
-            if board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X":
-                return True
-            elif board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X":
-                return True
-            elif board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X":
-                return True
-            elif board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X":
-                return True
-            elif board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X":
-                return True
-            elif board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X":
-                return True
-            elif board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X":
-                return True
-            elif board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X":
-                return True
-            else:
-                return False
+        if board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X":
+            return True
+        elif board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X":
+            return True
+        elif board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X":
+            return True
+        elif board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X":
+            return True
+        elif board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X":
+            return True
+        elif board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X":
+            return True
+        elif board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X":
+            return True
+        elif board[0][2] == "X" and board[1][1] == "X" and board[2][0] == "X":
+            return True
+        else:
+            return False
     if player == 2:
-        for space in board:
-            if board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O":
-                return True
-            elif board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O":
-                return True
-            elif board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O":
-                return True
-            elif board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O":
-                return True
-            elif board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O":
-                return True
-            elif board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O":
-                return True
-            elif board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
-                return True
-            elif board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
-                return True
-            else:
-                return False
+        if board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O":
+            return True
+        elif board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O":
+            return True
+        elif board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O":
+            return True
+        elif board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O":
+            return True
+        elif board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O":
+            return True
+        elif board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O":
+            return True
+        elif board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O":
+            return True
+        elif board[0][2] == "O" and board[1][1] == "O" and board[2][0] == "O":
+            return True
+        else:
+            return False
 
 
 def check_tie(board):
