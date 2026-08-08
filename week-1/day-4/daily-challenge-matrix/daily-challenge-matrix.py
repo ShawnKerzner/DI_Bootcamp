8fcf0bff60a7a6df4b755c8baae8ddb573f25aa6
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-08 15:59:35 +0300

    Initial outline of the functions with comments for their inteded purposes

---
+++ week-1/day-4/daily-challenge-matrix/daily-challenge-matrix.py
@@ -0,0 +1,18 @@
+def string_to_2D_list(coded_string):
+    pass
+# takes the matrix string, returns a 2D list
+
+
+def alpha_characters(the_list):
+    pass
+# takes a list, returns a string of just the alpha characters
+
+
+def symbol_to_spaces(the_list):
+    pass
+# takes a list, returns a list with symbol runs(between letters) replaced with spaces
+
+
+def decode(string):
+    pass
+# takes the raw matrix string, returns the final decoded message, using the functions above
