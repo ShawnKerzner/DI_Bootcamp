258cc832871e91057954cc8d7b285d54e33579ed
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-10 22:02:51 +0300

    exercise 2 complete

--- week-2/day-2/exercises-xp/exercises.xp.py
+++ week-2/day-2/exercises-xp/exercises.xp.py
@@ -1,43 +1,78 @@
-class Pets():
-    def __init__(self, animals):
-        self.animals = animals
+# # Exercise 1
+# class Pets():
+#     def __init__(self, animals):
+#         self.animals = animals
 
-    def walk(self):
-        for animal in self.animals:
-            print(animal.walk())
+#     def walk(self):
+#         for animal in self.animals:
+#             print(animal.walk())
 
 
-class Cat():
-    is_lazy = True
+# class Cat():
+#     is_lazy = True
 
-    def __init__(self, name, age):
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def walk(self):
+#         return f'{self.name} is just walking around'
+
+
+# class Bengal(Cat):
+#     def sing(self, sounds):
+#         return f'{sounds}'
+
+
+# class Chartreux(Cat):
+#     def sing(self, sounds):
+#         return f'{sounds}'
+
+
+# class Siamese(Cat):
+#     pass
+
+
+# toby = Bengal("Toby", 3)
+# crouton = Chartreux("Crouton", 5)
+# fuego = Siamese("Fuego", 1)
+
+# all_cats = [toby, crouton, fuego]
+
+# sara_pets = Pets(all_cats)
+
+# sara_pets.walk()
+
+# Exercise 2
+
+
+class Dog():
+    def __init__(self, name, age, weight):
         self.name = name
         self.age = age
+        self.weight = weight
 
-    def walk(self):
-        return f'{self.name} is just walking around'
+    def bark(self):
+        return f"{self.name} is barking"
 
+    def run_speed(self):
+        return self.weight / self.age * 10
 
-class Bengal(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-
-class Chartreux(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-
-class Siamese(Cat):
-    pass
+    def fight(self, other_dog):
+        my_stats = self.run_speed() * self.weight
+        foe_stats = other_dog.run_speed() * other_dog.weight
+        if my_stats > foe_stats:
+            return f"{self.name} wins"
+        else:
+            return f"{other_dog.name} wins"
 
 
-toby = Bengal("Toby", 3)
-crouton = Chartreux("Crouton", 5)
-fuego = Siamese("Fuego", 1)
+dog1 = Dog("Barcus", 12, 54)
+dog2 = Dog("Brodie", 6, 37)
+dog3 = Dog("Batman", 2, 45)
 
-all_cats = [toby, crouton, fuego]
+print(dog1.bark())
+print(dog2.run_speed())
+print(dog1.fight(dog2))
 
-sara_pets = Pets(all_cats)
-
-sara_pets.walk()
+# Exercise 3
