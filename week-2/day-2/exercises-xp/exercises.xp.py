549b773fdbd15e775a187b6f3688bc0becfc9264
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-13 00:04:46 +0300

    exercises complete

--- week-2/day-2/exercises-xp/exercises.xp.py
+++ week-2/day-2/exercises-xp/exercises.xp.py
@@ -47,49 +47,99 @@ import random
 # Exercise 2
 
 
-class Dog():
-    def __init__(self, name, age, weight):
-        self.name = name
+# class Dog():
+#     def __init__(self, name, age, weight):
+#         self.name = name
+#         self.age = age
+#         self.weight = weight
+
+#     def bark(self):
+#         return f"{self.name} is barking"
+
+#     def run_speed(self):
+#         return self.weight / self.age * 10
+
+#     def fight(self, other_dog):
+#         my_stats = self.run_speed() * self.weight
+#         foe_stats = other_dog.run_speed() * other_dog.weight
+#         if my_stats > foe_stats:
+#             return f"{self.name} wins"
+#         else:
+#             return f"{other_dog.name} wins"
+
+
+# dog1 = Dog("Barcus", 12, 54)
+# dog2 = Dog("Brodie", 6, 37)
+# dog3 = Dog("Batman", 2, 45)
+
+# print(dog1.bark())
+# print(dog2.run_speed())
+# print(dog1.fight(dog2))
+
+# # Exercise 3
+
+
+# class PetDog(Dog):
+#     def __init__(self, trained=False):
+#         self.trained = trained
+
+#     def train(self):
+#         self.trained = True
+#         return f"{self.bark()}"
+
+#     def play(self, *args):
+#         print(f"{args} all play together")
+
+#     def do_a_trick(self):
+#         if self.trained == True:
+#             print()
+
+# Exercise 4
+class Person():
+    def __init__(self, first_name, age):
+        self.first_name = first_name
         self.age = age
-        self.weight = weight
+        self.last_name = ""
 
-    def bark(self):
-        return f"{self.name} is barking"
-
-    def run_speed(self):
-        return self.weight / self.age * 10
-
-    def fight(self, other_dog):
-        my_stats = self.run_speed() * self.weight
-        foe_stats = other_dog.run_speed() * other_dog.weight
-        if my_stats > foe_stats:
-            return f"{self.name} wins"
+    def is_18(self):
+        if self.age >= 18:
+            return True
         else:
-            return f"{other_dog.name} wins"
+            return False
 
 
-dog1 = Dog("Barcus", 12, 54)
-dog2 = Dog("Brodie", 6, 37)
-dog3 = Dog("Batman", 2, 45)
+class Family():
+    def __init__(self, last_name):
+        self.last_name = last_name
+        self.members = []
 
-print(dog1.bark())
-print(dog2.run_speed())
-print(dog1.fight(dog2))
+    def born(self, first_name, age):
+        person = Person(first_name, age)
+        person.last_name = self.last_name
+        self.members.append(person)
 
-# Exercise 3
+    def check_majority(self, first_name):
+        for member in self.members:
+            if member.first_name == first_name:
+                if member.is_18() == True:
+                    print(
+                        "You are over 18, your parents Jane and John accept that you will go out with your friends")
+                else:
+                    print("Sorry, you are not allowed to go out with your friends."
+                          )
+
+    def family_presentation(self):
+        print(f"{self.last_name}")
+        for member in self.members:
+            print(f"{member.first_name}: {member.age}")
 
 
-class PetDog(Dog):
-    def __init__(self, trained=False):
-        self.trained = trained
-
-    def train(self):
-        self.trained = True
-        return f"{self.bark()}"
-
-    def play(self, *args):
-        print(f"{args} all play together")
-
-    def do_a_trick(self):
-        if self.trained == True:
-            print()
+family1 = Family("Kerzner")
+family1.born("Kevin", 65)
+family1.born("Rivkah", 62)
+family1.born("Ari", 34)
+family1.born("Erin", 29)
+family1.born("Shawn", 28)
+family1.born("Daniel", 27)
+family1.family_presentation()
+family1.check_majority("Shawn")
