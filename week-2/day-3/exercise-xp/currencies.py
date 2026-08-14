f7157e0d5536eaa0537a83192af126c36b7904f4
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-14 12:55:17 +0300

    currencies exercise completed

--- week-2/day-3/exercise-xp/currencies.py
+++ week-2/day-3/exercise-xp/currencies.py
@@ -20,4 +20,53 @@ class Currency:
                 return self.amount + other.amount
             else:
                 raise TypeError(
-                    f"Cannot add between Currency type <dollar> and <shekel>")
+                    f"Cannot add between Currency type <{self.currency}> and <{other.currency}>")
+
+    def __iadd__(self, other):
+        if isinstance(other, Currency):
+            if self.currency == other.currency:
+                new_amount = self.amount + other.amount
+                self.amount = new_amount
+                return self
+            else:
+                raise TypeError(
+                    f"Cannot add between Currency type <{self.currency}> and <{other.currency}>")
+        else:
+            new_amount = self.amount + other
+            self.amount = new_amount
+            return self
+
+
+c1 = Currency('dollar', 5)
+c2 = Currency('dollar', 10)
+c3 = Currency('shekel', 1)
+c4 = Currency('shekel', 10)
+
+print(c1)
+# '5 dollars'
+
+print(int(c1))
+# 5
+
+print(repr(c1))
+# '5 dollars'
+
+print(c1 + 5)
+# 10
+
+print(c1 + c2)
+# 15
+
+print(c1)
+# 5 dollars
+
+c1 += 5
+print(c1)
+# 10 dollars
+
+c1 += c2
+print(c1)
+# 20 dollars
+
+print(c1 + c3)
+# TypeError: Cannot add between Currency type <dollar> and <shekel>
