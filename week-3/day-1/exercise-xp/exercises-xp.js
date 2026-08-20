ac6977ebd78e6cb530b168b00219e6727f4054a7
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-20 16:26:10 +0300

    All exercises completed

--- week-3/day-1/exercise-xp/exercises-xp.js
+++ week-3/day-1/exercise-xp/exercises-xp.js
@@ -1,95 +1,95 @@
-// const readlineSync = require('readline-sync'); // for exercise 3
-// // Exercise 1
-// const people = ["Greg", "Mary", "Devon", "James"];
-// people.splice(0, 1);
-// people.splice(2, 3, 'Jason');
-// people.push("Shawn");
-// console.log(people.indexOf("Mary"));
-// const copyPeople = people.slice(1, -1);
-// console.log(people.indexOf("Foo")); // It returns negative 1 because the item 'Foo' is not an item in the list
+const readlineSync = require('readline-sync'); // for exercise 3
+// Exercise 1
+const people = ["Greg", "Mary", "Devon", "James"];
+people.splice(0, 1);
+people.splice(2, 3, 'Jason');
+people.push("Shawn");
+console.log(people.indexOf("Mary"));
+const copyPeople = people.slice(1, -1);
+console.log(people.indexOf("Foo")); // It returns negative 1 because the item 'Foo' is not an item in the list
 
-// for (const item of people) {
-//     console.log(item)
-// };
+for (const item of people) {
+    console.log(item)
+};
 
-// for (const item of people) {
-//     if (item == "Devon") {
-//         console.log(item);
-//         break;
-//     }
-//     else {
-//         console.log(item);
-//         break;
-//     }
-// };
+for (const item of people) {
+    if (item == "Devon") {
+        console.log(item);
+        break;
+    }
+    else {
+        console.log(item);
+        break;
+    }
+};
 
-// // Exercise 2
-// const colors = ["navy", "black", "white", "beige", "brown"]
-// const suffixes = ["st", "nd", "rd", "th", "th"]
-// for (let i = 0; i < colors.length; i++) {
-//     console.log("My #" + (i + 1) + " choice is " + colors[i])
-// }
+// Exercise 2
+const colors = ["navy", "black", "white", "beige", "brown"]
+const suffixes = ["st", "nd", "rd", "th", "th"]
+for (let i = 0; i < colors.length; i++) {
+    console.log("My #" + (i + 1) + " choice is " + colors[i])
+}
 
-// for (let i = 0; i < colors.length; i++) {
-//     console.log("My " + (i + 1) + suffixes[i] + " choice is " + colors[i])
-// }
+for (let i = 0; i < colors.length; i++) {
+    console.log("My " + (i + 1) + suffixes[i] + " choice is " + colors[i])
+}
 
-// // Exercise 3
+// Exercise 3
 
-// do {
-//     let user_input = readlineSync.question("Enter a number equal to or greater than 10 to exit the loop.");
-//     let user_number = Number(user_input);
-//     if (isNaN(user_number)) {
-//         console.log("Input must be a number");
-//         continue
-//     } else {
-//         if (user_number < 10) {
-//             continue
-//         } else {
-//             break
-//         }
-//     }
-// } while (1);
+do {
+    let user_input = readlineSync.question("Enter a number equal to or greater than 10 to exit the loop.");
+    let user_number = Number(user_input);
+    if (isNaN(user_number)) {
+        console.log("Input must be a number");
+        continue
+    } else {
+        if (user_number < 10) {
+            continue
+        } else {
+            break
+        }
+    }
+} while (1);
 
-// // Exercise 4
-// const building = {
-//     numberOfFloors: 4,
-//     numberOfAptByFloor: {
-//         firstFloor: 3,
-//         secondFloor: 4,
-//         thirdFloor: 9,
-//         fourthFloor: 2,
-//     },
-//     nameOfTenants: ["Sarah", "Dan", "David"],
-//     numberOfRoomsAndRent: {
-//         sarah: [3, 990],
-//         dan: [4, 1000],
-//         david: [1, 500],
-//     },
-// }
-// console.log(building.numberOfFloors);
-// console.log(building.numberOfAptByFloor.firstFloor, building.numberOfAptByFloor.thirdFloor);
-// console.log(building.nameOfTenants[1], building.numberOfRoomsAndRent.dan[0]);
-// if (building.numberOfRoomsAndRent.sarah[1] + building.numberOfRoomsAndRent.david[1] > building.numberOfRoomsAndRent.dan[1]) {
-//     building.numberOfRoomsAndRent.dan[1] = 1200
-// };
-// console.log(building.numberOfRoomsAndRent.dan[1])
+// Exercise 4
+const building = {
+    numberOfFloors: 4,
+    numberOfAptByFloor: {
+        firstFloor: 3,
+        secondFloor: 4,
+        thirdFloor: 9,
+        fourthFloor: 2,
+    },
+    nameOfTenants: ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent: {
+        sarah: [3, 990],
+        dan: [4, 1000],
+        david: [1, 500],
+    },
+}
+console.log(building.numberOfFloors);
+console.log(building.numberOfAptByFloor.firstFloor, building.numberOfAptByFloor.thirdFloor);
+console.log(building.nameOfTenants[1], building.numberOfRoomsAndRent.dan[0]);
+if (building.numberOfRoomsAndRent.sarah[1] + building.numberOfRoomsAndRent.david[1] > building.numberOfRoomsAndRent.dan[1]) {
+    building.numberOfRoomsAndRent.dan[1] = 1200
+};
+console.log(building.numberOfRoomsAndRent.dan[1])
 
-// // Exercise 5
-// const family = {
-//     Dad: 65,
-//     Mom: 62,
-//     Ari: 34,
-//     Erin: 29,
-//     Shawn: 28,
-//     Daniel: 27,
-// }
-// for (let key in family) {
-//     console.log(key)
-// }
-// for (let key in family) {
-//     console.log(family[key])
-// }
+// Exercise 5
+const family = {
+    Dad: 65,
+    Mom: 62,
+    Ari: 34,
+    Erin: 29,
+    Shawn: 28,
+    Daniel: 27,
+}
+for (let key in family) {
+    console.log(key)
+}
+for (let key in family) {
+    console.log(family[key])
+}
 
 // Exercise 6
 const details = {
@@ -104,3 +104,15 @@ for (let arrayItems of Object.entries(details)) {
 }
 console.log(str);
 
+// Exercise 7
+
+const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
+const secretSociety = []
+for (let nam3 of names) {
+    secretSociety.push(nam3[0])
+}
+console.log(secretSociety)
+const sortedName = secretSociety.sort()
+console.log(sortedName)
+const realSortedName = sortedName.join("")
+console.log(realSortedName)
