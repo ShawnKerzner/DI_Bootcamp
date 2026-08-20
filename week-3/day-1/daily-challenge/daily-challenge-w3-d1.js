a776e96c23a670c458b83ffa307a2165b3315aa8
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-20 10:50:00 +0300

    refactor complete

--- week-3/day-1/daily-challenge/daily-challenge-w3-d1.js
+++ week-3/day-1/daily-challenge/daily-challenge-w3-d1.js
@@ -1,12 +1,29 @@
-const sentence = "The movie is not that bad, I like it.";
-const wordNot = sentence.indexOf("not");
-const wordBad = sentence.indexOf("bad");
-if (wordBad > wordNot) {
-    const goodSwitch = "The movie is good, I like it."
-    console.log(goodSwitch)
+// const sentence = "The movie is not that bad, I like it.";
+// const wordNot = sentence.indexOf("not");
+// const wordBad = sentence.indexOf("bad");
+// if (wordBad > wordNot) {
+//     const goodSwitch = "The movie is good, I like it."
+//     console.log(goodSwitch)
+// } else {
+//     console.log(sentence)
+// };
+
+// Refactor 
+let sentence = "The movie is not that bad, I like it.";
+let sentenceLower = sentence.toLowerCase();
+const start = "not"
+const end = "bad"
+let wordNot = sentenceLower.indexOf(start);
+let wordBad = sentenceLower.indexOf(end);
+
+if (wordNot < wordBad || false) {
+    console.log(sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + end.length));
 } else {
-    console.log(sentence)
-};
+    console.log(sentence);
+}
 
-
-
+// if (wordBad > wordNot) {
+//     console.log(sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + end.length));
+// } else {
+//     console.log(sentence);
+// }
