293fd61ecc78f0e0cb037e9259116022d4fa050d
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-20 09:17:59 +0300

    Exercise 3 complete

--- week-3/day-1/exercise-xp/exercises-xp.js
+++ week-3/day-1/exercise-xp/exercises-xp.js
@@ -1,3 +1,4 @@
+const readlineSync = require('readline-sync');
 // // Exercise 1
 // const people = ["Greg", "Mary", "Devon", "James"];
 // people.splice(0, 1);
@@ -22,14 +23,30 @@
 //     }
 // };
 
-// Exercise 2
-const colors = ["navy", "black", "white", "beige", "brown"]
-const suffixes = ["st", "nd", "rd", "th", "th"]
-for (let i = 0; i < colors.length; i++) {
-    console.log("My #" + (i + 1) + " choice is " + colors[i])
-}
+// // Exercise 2
+// const colors = ["navy", "black", "white", "beige", "brown"]
+// const suffixes = ["st", "nd", "rd", "th", "th"]
+// for (let i = 0; i < colors.length; i++) {
+//     console.log("My #" + (i + 1) + " choice is " + colors[i])
+// }
 
-for (let i = 0; i < colors.length; i++) {
-    console.log("My " + (i + 1) + suffixes[i] + " choice is " + colors[i])
-}
+// for (let i = 0; i < colors.length; i++) {
+//     console.log("My " + (i + 1) + suffixes[i] + " choice is " + colors[i])
+// }
 
+// Exercise 3
+
+do {
+    let user_input = readlineSync.question("Enter a number equal to or greater than 10 to exit the loop.");
+    let user_number = Number(user_input);
+    if (isNaN(user_number)) {
+        console.log("Input must be a number");
+        continue
+    } else {
+        if (user_number < 10) {
+            continue
+        } else {
+            break
+        }
+    }
+} while (1);
