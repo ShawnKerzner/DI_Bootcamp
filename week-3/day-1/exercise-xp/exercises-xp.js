ce46b6f9388812ae6210cb3f659f0ae99803105f
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-20 15:21:52 +0300

    exercise 4 completet

--- week-3/day-1/exercise-xp/exercises-xp.js
+++ week-3/day-1/exercise-xp/exercises-xp.js
@@ -1,4 +1,4 @@
-const readlineSync = require('readline-sync');
+// const readlineSync = require('readline-sync'); // for exercise 3
 // // Exercise 1
 // const people = ["Greg", "Mary", "Devon", "James"];
 // people.splice(0, 1);
@@ -34,19 +34,43 @@ const readlineSync = require('readline-sync');
 //     console.log("My " + (i + 1) + suffixes[i] + " choice is " + colors[i])
 // }
 
-// Exercise 3
+// // Exercise 3
 
-do {
-    let user_input = readlineSync.question("Enter a number equal to or greater than 10 to exit the loop.");
-    let user_number = Number(user_input);
-    if (isNaN(user_number)) {
-        console.log("Input must be a number");
-        continue
-    } else {
-        if (user_number < 10) {
-            continue
-        } else {
-            break
-        }
-    }
-} while (1);
+// do {
+//     let user_input = readlineSync.question("Enter a number equal to or greater than 10 to exit the loop.");
+//     let user_number = Number(user_input);
+//     if (isNaN(user_number)) {
+//         console.log("Input must be a number");
+//         continue
+//     } else {
+//         if (user_number < 10) {
+//             continue
+//         } else {
+//             break
+//         }
+//     }
+// } while (1);
+
+// Exercise 4
+const building = {
+    numberOfFloors: 4,
+    numberOfAptByFloor: {
+        firstFloor: 3,
+        secondFloor: 4,
+        thirdFloor: 9,
+        fourthFloor: 2,
+    },
+    nameOfTenants: ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent: {
+        sarah: [3, 990],
+        dan: [4, 1000],
+        david: [1, 500],
+    },
+}
+console.log(building.numberOfFloors);
+console.log(building.numberOfAptByFloor.firstFloor, building.numberOfAptByFloor.thirdFloor);
+console.log(building.nameOfTenants[1], building.numberOfRoomsAndRent.dan[0]);
+if (building.numberOfRoomsAndRent.sarah[1] + building.numberOfRoomsAndRent.david[1] > building.numberOfRoomsAndRent.dan[1]) {
+    building.numberOfRoomsAndRent.dan[1] = 1200
+};
+console.log(building.numberOfRoomsAndRent.dan[1])
