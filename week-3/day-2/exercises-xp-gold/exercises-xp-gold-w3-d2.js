5c786402eaedf660e745e339067f081553f65347
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-23 10:07:24 +0300

    Exercises 1 and 2 completed. Challenge still in progress 2/5 done

---
+++ week-3/day-2/exercises-xp-gold/exercises-xp-gold-w3-d2.js
@@ -0,0 +1,26 @@
+// // Exercise 1
+// function isBlank(string = "") {
+//     if (string == "") {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// };
+
+// console.log(isBlank());
+// console.log(isBlank("Hello World"));
+
+// // Exercise 2
+// function abbrevName(first_name = "John", last_name = "Doe") {
+//     return first_name + " " + last_name[0] + ".";
+// }
+
+// console.log(abbrevName())
+
+// // Exercise 3
+// function swapCase (sentence) {
+
+// }
+
+// Exercise 5
---
+++ week-3/day-2/exercises-xp-gold/script.js
@@ -0,0 +1,1 @@
+// Javascript code for exercise 5
