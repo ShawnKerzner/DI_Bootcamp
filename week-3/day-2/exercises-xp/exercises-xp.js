22fd5bfd490ec83165691fcaed8624fd98860cae
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-25 21:09:48 +0300

    Exercise 3 complete. Exercise 4 in progress

--- week-3/day-2/exercises-xp/exercises-xp.js
+++ week-3/day-2/exercises-xp/exercises-xp.js
@@ -1,4 +1,4 @@
-// Exercise 1
+// // Exercise 1
 // function displayNumberDivisible(divisor) {
 //     for (i = 0; i < 500; i++) {
 //         if (i % divisor == 0) {
@@ -9,36 +9,91 @@
 
 // displayNumberDivisible(56)
 
-// Exercise 2
-const stock = {
-    "banana": 6,
-    "apple": 0,
-    "pear": 12,
-    "orange": 32,
-    "blueberry": 1
-};
+// // Exercise 2
+// const stock = {
+//     "banana": 6,
+//     "apple": 0,
+//     "pear": 12,
+//     "orange": 32,
+//     "blueberry": 1
+// };
 
-const prices = {
-    "banana": 4,
-    "apple": 2,
-    "pear": 1,
-    "orange": 1.5,
-    "blueberry": 10
-};
+// const prices = {
+//     "banana": 4,
+//     "apple": 2,
+//     "pear": 1,
+//     "orange": 1.5,
+//     "blueberry": 10
+// };
 
-const shoppingList = ["banana", "orange", "apple"];
-let totalPrice = 0
+// const shoppingList = ["banana", "orange", "apple"];
+// let totalPrice = 0
 
-function myBill() {
-    for (item of shoppingList) {
-        if (item in stock) {
-            if (stock[item] > 0) {
-                stock[item] = stock[item] - 1;
-                totalPrice += prices[item];
-            }
+// function myBill() {
+//     for (item of shoppingList) {
+//         if (item in stock) {
+//             if (stock[item] > 0) {
+//                 stock[item] = stock[item] - 1;
+//                 totalPrice += prices[item];
+//             }
+//         }
+//     }
+//     console.log(totalPrice);
+// };
+
+// myBill()
+
+// // Exercise 3
+// function changeEnough(itemPrice, amountOfChange) {
+//     let sumOfChange = 0.0;
+//     const coins = [.25, .10, .05, .01];
+//     for (let i = 0; i < 4; i++) {
+//         sumOfChange += (amountOfChange[i] * coins[i]);
+//     }
+//     if (sumOfChange > itemPrice) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// console.log(changeEnough(14.11, [2, 100, 0, 0])) // we need false
+// console.log(changeEnough(0.75, [0, 0, 20, 5]))  // we need true
+
+// Exercise 4
+// function hotelCost() {
+//     while (a = 1) {
+//         const numOfNights = prompt("How many nights would you like to stay?");
+//         if (isNaN(numOfNights)) {
+//             continue
+//         } else {
+//             if (numOfNights == "") {
+//                 numOfNights = 0;
+//             }
+//             totalHotelCost = (numOfNights * 140);
+//             console.log(totalHotelCost);
+//             return totalHotelCost
+//         }
+//     }
+// }
+
+// hotelCost()
+
+function planeRideCost() {
+    ticketPrice = 0
+    while (b = 2) {
+        const destination = prompt("What is your destination?").toLowerCase();
+        if (destination == "") {
+            continue
+        } else if (isNaN(destination) == false) {
+            continue
+        } else if (destination == "london") {
+            ticketPrice += 183;
+            console.log(ticketPrice);
+            return ticketPrice
         }
     }
-    console.log(totalPrice);
-};
+}
 
-myBill()
+
+planeRideCost()
