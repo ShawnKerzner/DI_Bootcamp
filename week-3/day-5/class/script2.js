6b1791f2573997e61c33e1665a7f2f66a77ad114
Shawn Kerzner <shawnkerz98@gmail.com>
2026-08-26 10:05:27 +0300

    more notes

---
+++ week-3/day-5/class/script2.js
@@ -0,0 +1,12 @@
+const myArray = [1, 2, 3, 4, 5];
+
+// myArray.push(6);
+// let a = myArray.pop();
+// let b = myArray.shift();
+// myArray.unshift(-1);
+// console.log(myArray);
+
+for (let i = 0; i < myArray.length; i++) {
+    myArray[i]++
+}
+console.log(myArray)
