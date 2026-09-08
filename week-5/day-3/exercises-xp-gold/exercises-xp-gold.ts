e5841da756183a579e4f4aa4f2c1bd9b37978d8c
Shawn Kerzner <shawnkerz98@gmail.com>
2026-09-08 18:02:59 +0300

    all exercises complete

--- week-5/day-3/exercises-xp-gold/exercises-xp-gold.ts
+++ week-5/day-3/exercises-xp-gold/exercises-xp-gold.ts
@@ -1,29 +1,67 @@
-// Exercise 1 
-function processValue(input: string | number): void {
-    if(typeof input === "number") {
-        console.log(`$${input}.00`);
-    } else {
-        const reversed: string = input.split("").reverse().join("");
-        console.log(reversed)
-    }
-}
-processValue(12);
-processValue("Hello");
+// // Exercise 1 
+// function processValue(input: string | number): void {
+//     if(typeof input === "number") {
+//         console.log(`$${input}.00`);
+//     } else {
+//         const reversed: string = input.split("").reverse().join("");
+//         console.log(reversed)
+//     }
+// }
+// processValue(12);
+// processValue("Hello");
 
 // // Exercise 2
-// function sumNumbersInArray(myArray: string | number): string {
+// function sumNumbersInArray(myArray: (string | number) []): number {
 //     let result: number = 0
-//     for(let item of myArray)
+//     for(let item of myArray) {
+//         if (typeof item === "number") {
+//             result += item
+//         }
+//     }
+//     return result;
 // }
 
-// Exercise 4
-function welcomeUser(name: string, greeting?: boolean ): void {
-    if (greeting === undefined) {
-        console.log("Hello");
+// console.log(sumNumbersInArray([1,"hi", 4, "bye", 5]));
+
+// Exercise 3
+
+type AdvancedUser = {
+    name: string;
+    age: number;
+    address?: string;
+}
+
+function introduceAdvancedUser(obj: AdvancedUser) {
+    if (obj["address"] !== undefined) {
+        return `Hello! My name is ${obj["name"]}. I am ${obj["age"]} years old. My address is ${obj.address}.`;
     } else {
-        console.log(`Good morning ${name}! I hope you have an amazing day!`)
+        return `Hello! My name is ${obj["name"]}. I am ${obj["age"]} years old.`;
     }
 }
 
-welcomeUser("Shawn");
-welcomeUser("Shawn", true)
+const userWithAddress: AdvancedUser = {
+  name: "Alice",
+  age: 30,
+  address: "123 Main St"
+};
+
+const userWithoutAddress: AdvancedUser = {
+  name: "Bob",
+  age: 25
+};
+
+console.log(introduceAdvancedUser(userWithAddress));
+console.log(introduceAdvancedUser(userWithoutAddress));
+
+
+// // Exercise 4
+// function welcomeUser(name: string, greeting?: boolean ): void {
+//     if (greeting === undefined) {
+//         console.log("Hello");
+//     } else {
+//         console.log(`Good morning ${name}! I hope you have an amazing day!`)
+//     }
+// }
+
+// welcomeUser("Shawn");
+// welcomeUser("Shawn", true)
