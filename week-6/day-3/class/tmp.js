e774cf8ae6f28b30ff067dc76aac33141f530a01
Shawn Kerzner <shawnkerz98@gmail.com>
2026-09-15 12:34:10 +0300

    notes and exercises from todays lesson

---
+++ week-6/day-3/class/tmp.js
@@ -0,0 +1,14 @@
+// const name = "John Doe";
+
+// export const greeting = function (name) {
+//     console.log(`Hello world and ${name}`);
+// }
+
+// const bye = function (name) {
+//     console.log(`Goodbye ${name}`);
+// }
+
+// const confusion = "I'm confused";
+
+// module.exports = {greeting, bye, confusion};
+
---
+++ week-6/day-3/class/tmp2.js
@@ -0,0 +1,7 @@
+// const myTmp = require("./tmp");
+// import { greeting } from "./tmp.js";
+// const myName = "John";
+// greeting(myName)
+// myTmp.greeting(myName);
+// myTmp.bye(myName);
+// console.log(myTmp.confusion);
