b4efbfa6bc91c9a57dc381eff012cf06fa9195a5
Shawn Kerzner <shawnkerz98@gmail.com>
2026-09-20 21:25:57 +0300

    saving work

---
+++ week-6/day-4/daily-challenge/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+let currentAnswer = null;
+let currentPlayer = {
+    score:0
+};
+
+const emojis = [
+    { emoji: '😀', name: 'Smile' },
+    { emoji: '🐶', name: 'Dog' },
+    { emoji: '🌮', name: 'Taco' }
+];
+
+app.use(express.json());
+
+app.listen(5000, () => {
+    console.log("server is listening on port 5000...")
+});
+
+app.get('/round', (req, res) => {
+    const randomNumber = Math.floor((Math.random() * emojis.length));
+    const randomEmoji = emojis[randomNumber];
+    const wrongAnswers = emojis.filter((emoji) => {
+    return (emoji.name != randomEmoji.name)
+    })
+    const randomIndex = Math.floor(Math.random() * (wrongAnswers.length + 1));
+    wrongAnswers.splice(randomIndex, 0, randomEmoji);
+    const answersShuffled = wrongAnswers
+    currentAnswer = randomEmoji;
+    const data = {
+        emoji: currentAnswer.emoji,
+        options: answersShuffled
+    }
+    res.status(200).json(data)
+})
+
+app.post('/guess', (req, res) => {
+    const guess = req.body.guess;
+    if (guess === currentAnswer.name){
+        currentPlayer.score += 1;
+        res.status(200).json()
+    } else {
+
+    }
+})
