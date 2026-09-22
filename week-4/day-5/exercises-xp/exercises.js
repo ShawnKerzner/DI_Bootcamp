37802475ea87e598e207bafb30fcdbb86eac3f91
Shawn Kerzner <shawnkerz98@gmail.com>
2026-09-22 15:51:50 +0300

    all exercises complete

--- week-4/day-5/exercises-xp/exercises.js
+++ week-4/day-5/exercises-xp/exercises.js
@@ -1,21 +1,21 @@
-// // Exercises 1
-// function compareToTen(num) {
-//     return new Promise ((resolve, reject) => {
-//         if(num <= 10) {
-//             resolve(`Success: ${num} is less than or equal to 10`);
-//         } else {
-//             reject(`Error: ${num} is greater than 10`);
-//         }
-//     });   
-// }
+// Exercises 1
+function compareToTen(num) {
+    return new Promise ((resolve, reject) => {
+        if(num <= 10) {
+            resolve(`Success: ${num} is less than or equal to 10`);
+        } else {
+            reject(`Error: ${num} is greater than 10`);
+        }
+    });   
+}
 
-// compareToTen(12)
-//    .then(result => console.log(result))
-//    .catch(error => console.log(error));
+compareToTen(12)
+   .then(result => console.log(result))
+   .catch(error => console.log(error));
 
-// compareToTen(8)
-//     .then(result => console.log(result))
-//    .catch(error => console.log(error));
+compareToTen(8)
+    .then(result => console.log(result))
+   .catch(error => console.log(error));
 
 // Exercises 2
 
@@ -26,3 +26,19 @@ function fourSecondTimer() {
 }
 
 fourSecondTimer().then(result => console.log(result));
+
+// Exercise 3
+
+function resolveReject(value) {
+    return new Promise ((resolve, reject) => {
+        if(value === 3) {
+            resolve(3)
+        } else {
+            reject("Boo!")
+        }
+    })
+}
+
+resolveReject(3)
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
